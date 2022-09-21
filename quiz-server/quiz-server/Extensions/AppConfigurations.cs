d0c261b914900e78568d22949f4d5170bea93c22
BlagoKolev <blagovest.kolev@gmail.com>
2022-09-21 11:41:32 +0300

    Create auto seed Questions table in database.

--- quiz-server/quiz-server/Extensions/AppConfigurations.cs
+++ quiz-server/quiz-server/Extensions/AppConfigurations.cs
@@ -14,10 +14,24 @@ namespace quiz_server.Extensions
 
             MigrateDatabase(services);
             SeedAuthors(services);
+            SeedQuestions(services);
 
             return app;
         }
+        private static async void SeedQuestions(IServiceProvider services)
+        {
+            var db = GetDbContext(services);
+            if (db.Questions.Any())
+            {
+                return;
+            }
 
+            using FileStream openStream = File.OpenRead("Resources/Questions.json");
+            var questions = await JsonSerializer.DeserializeAsync<Question[]>(openStream);
+
+            await db.Questions.AddRangeAsync(questions);
+            await db.SaveChangesAsync();
+        }
         private static async void SeedAuthors(IServiceProvider services)
         {
             var db = GetDbContext(services);
@@ -32,7 +46,6 @@ namespace quiz_server.Extensions
 
             await db.Authors.AddRangeAsync(authors);
             await db.SaveChangesAsync();
-           
         }
         private static QuizDbContext GetDbContext(IServiceProvider services)
         {
