020b22f1980ecac26e4f901faf08b8881974919c
BlagoKolev <blagovest.kolev@gmail.com>
2022-09-27 22:39:32 +0300

    Set a notification of the result of the user's answer. (using MUI).

--- quiz-client/src/components/Question.js
+++ quiz-client/src/components/Question.js
@@ -1,11 +1,13 @@
 import Center from './Center';
 import { useCallback, useEffect, useState } from 'react';
 import { BaseUrl, api, questionEndPoint, setScore } from '../Constants/Constants';
-import { Box, Card, CardContent, Typography, List, ListItem, ListItemText, Divider, Button, Alert } from '@mui/material';
+import { Box, Card, CardContent, Typography, List, ListItem, ListItemText, Divider, Button, Snackbar, Alert } from '@mui/material';
 
 function Question() {
 
     let [question, setQuestion] = useState({});
+    let [notification, setNotification] = useState(false);
+    let [notificationMsg, setNotificationMsg] = useState('');
 
     let token = localStorage.getItem('token');
 
@@ -25,16 +27,20 @@ function Question() {
                 body: JSON.stringify(sendData)
             })
             .then(res => res.json())
-            .then(res => alert(`Your score is ${res}`))
+            .then(res => {
+                setNotification(true)
+                setNotificationMsg(`Correct answer. You just won ${question.pointsReward} points. Total score: ${res}`);
+            })
             .catch(console.error);
     }
 
     const checkAnswer = (value) => () => {
+
         if (value == question.answerId) {
-            alert("Correct Answer")
             addPointsToUserScore();
         } else {
-            alert("Wrong Answer")
+            setNotificationMsg('Wrong anser.');
+            setNotification(true);
         }
         getQuestion();
     }
@@ -60,6 +66,9 @@ function Question() {
             .catch(console.error);
     }, [getQuestion]);
 
+    const closeSnackbar = () => {
+        setNotification(false);
+    }
     console.log(question)
 
     return (
@@ -69,16 +78,23 @@ function Question() {
                     <Typography variant='h4' sx={{ my: 3 }}>{question.text}</Typography>
                     <Box sx={{ '.MuiFormControl-root': { m: 1, width: '90%' } }}>
                         <List component="nav" aria-label="mailbox folders">
-                            {question.options ? question.options.map(x =>
-                                <div key={x.id}>
-                                    <ListItem button onClick={checkAnswer(x.id)}>
-                                        <ListItemText primary={x.name} />
-                                    </ListItem>
-                                    <Divider />
-                                </div>
-                            )
+                            {question.options
+                                ? question.options.map(x =>
+                                    <div key={x.id}>
+                                        <ListItem button onClick={checkAnswer(x.id)}>
+                                            <ListItemText primary={x.name} />
+                                        </ListItem>
+                                        <Divider />
+                                    </div>)
                                 : ''}
                         </List>
+                        {notification && <Snackbar
+                            open={notification}
+                            autoHideDuration={3000}
+                            onClose={closeSnackbar}>
+                            <Alert severity='info'>{notificationMsg}</Alert>
+                        </Snackbar>}
+
                     </Box>
                 </CardContent>
             </Card>
